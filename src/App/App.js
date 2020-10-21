1ec394dc2f301295ee1ffd20519d643c8bf41dae
Nathan Darrington <npdarrington@icloud.com>
2020-10-21 09:33:20 -0600

    Added conditional render if no reservations are found and initial render for reservations when found

--- src/App/App.js
+++ src/App/App.js
@@ -11,11 +11,19 @@ export default class App extends Component {
 	}
 
 	render() {
+    const { reservations } = this.state;
 		return (
 			<div className='App'>
 				<h1 className='app-title'>Turing Cafe Reservations</h1>
 				<div className='resy-form'></div>
-				<div className='resy-container'></div>
+				<div className='resy-container'>
+          {reservations.length < 1 && <h2>There are no reservations currently</h2>}
+          {
+            reservations.map(reservations => 
+              <h3>Reservation!</h3>  
+            )
+          }
+        </div>
 			</div>
 		);
 	}
