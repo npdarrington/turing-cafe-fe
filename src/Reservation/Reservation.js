cd0ea3e2770f27f4af13f9f151df34fea2b0f183
Nathan Darrington <npdarrington@icloud.com>
2020-10-21 10:04:38 -0600

    Completed Reservation component with styling

---
+++ src/Reservation/Reservation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './Reservation.css';
+
+const Reservation = ({ reservation }) => {
+	return (
+		<article className='reservation'>
+			<h2 className='reservation-name'>{reservation.name}</h2>
+			<p className='reservation-date'>{reservation.date}</p>
+			<p className='reservation-time'>{reservation.time} PM</p>
+			<h4 className='reservation-guests'>
+				Numbers of guests: {reservation.number}
+			</h4>
+			<button type='button' name='cancel-reservation'>
+				Cancel
+			</button>
+		</article>
+	);
+};
+
+export default Reservation;
