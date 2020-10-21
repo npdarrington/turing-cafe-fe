7ab379e5599596105d8e11f997b8bf305767a45a
Nathan Darrington <npdarrington@icloud.com>
2020-10-21 10:39:32 -0600

    Added imports for Form test and default test placeholders

---
+++ src/Form/Form.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import userEvent from '@testing-library/user-event'
+import { screen, render, waitFor } from '@testing-library/react';
+
+import Form from './Form';
+
+describe('Form', () => {
+  it('Should allow a user to record data into form inputs', () => {
+    
+  });
+
+  it('Should allow the user to add a new reservation', () => {
+
+  });
+})
