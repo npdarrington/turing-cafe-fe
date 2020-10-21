21d972597e0819883425e4d5026b698fc22165c9
Nathan Darrington <npdarrington@icloud.com>
2020-10-21 11:05:33 -0600

    A user should be allowed ot add a new reservation test and removed default test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import userEvent from '@testing-library/user-event';
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('Should allow the user to add a new reservation', () => {
+	render(<App />);
+	const name = screen.getByPlaceholderText('Name');
+	const date = screen.getByPlaceholderText('Date M/D');
+	const time = screen.getByPlaceholderText('Time H:MM');
+	const guests = screen.getByPlaceholderText('Number of guests');
+	userEvent.type(name, 'Nathan');
+	userEvent.type(date, '10/22');
+	userEvent.type(time, '3:30');
+  userEvent.type(guests, '4');
+	userEvent.click(screen.getByRole('button', { name: 'Make Reservation' }));
+	const reservationName = screen.getByText('Nathan');
+	const reservationTime = screen.getByText('3:30 PM');
+	expect(reservationName).toBeInTheDocument();
+	expect(reservationTime).toBeInTheDocument();
 });
