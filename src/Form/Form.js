a39610e15304aa5bb76cf2e8447ed5004327960c
Nathan Darrington <npdarrington@icloud.com>
2020-10-21 10:16:01 -0600

    Added updateHandler for handling state changes and formatted document for readability

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,31 +1,60 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 import './Form.css';
 
 export default class Form extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      name: '',
-      date: '',
-      time: '',
-      guests: ''
-    }
-  }
+		this.state = {
+			name: '',
+			date: '',
+			time: '',
+			guests: '',
+		};
+	}
 
-  render() {
-    return (
-      <form class="form">
-        <label htmlFor='name'></label>
-        <input type='text' name='name' placeholder='Name' />
-        <label htmlFor='date'></label>
-        <input type='date' name='date' placeholder='Date' />
-        <label htmlFor='time'></label>
-        <input type='time' name='time' placeholder='Time' />
-        <label htmlFor='guests'></label>
-        <input type='number' name='guests' placeholder='Number of guests' />
-      </form>
-    )
-  }
+	updateHandler = event => {
+		const { name, value } = event.target;
+		this.setState({ [name]: value });
+	};
+
+	render() {
+		return (
+			<form class='form'>
+				<label htmlFor='name'></label>
+				<input
+					type='text'
+					name='name'
+					placeholder='Name'
+					value={this.state.name}
+					onChange={this.updateHandler}
+				/>
+				<label htmlFor='date'></label>
+				<input
+					type='date'
+					name='date'
+					placeholder='Date'
+					value={this.state.date}
+					onChange={this.updateHandler}
+				/>
+				<label htmlFor='time'></label>
+				<input
+					type='time'
+					name='time'
+					placeholder='Time'
+					value={this.state.time}
+					onChange={this.updateHandler}
+				/>
+				<label htmlFor='guests'></label>
+				<input
+					type='number'
+					name='guests'
+					placeholder='Number of guests'
+					value={this.state.guests}
+					onChange={this.updateHandler}
+				/>
+			</form>
+		);
+	}
 }
