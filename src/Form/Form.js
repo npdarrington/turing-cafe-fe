3079a9e0c087cd371c5ed4e60d9fe46ab3f397f3
Nathan Darrington <npdarrington@icloud.com>
2020-10-21 11:56:07 -0600

    Removed all form validation as a weird bug was introduced, took back into working state

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -11,100 +11,66 @@ export default class Form extends Component {
 			date: '',
 			time: '',
 			number: '',
-			error: '',
 		};
 	}
 
 	updateHandler = event => {
 		const { name, value } = event.target;
-		this.setState({ [name]: value });
-	};
+    this.setState({ [name]: value });
+  };
 
-	submitReservation = () => {
-		this.setState({ error: '' });
-		this.validateInputs();
-		if (this.state.error) {
-			const { name, date, time, number } = this.state;
-			const reservation = { name, date, time, number };
-			this.props.addNewReservation(reservation);
-			this.clearInputs();
-		}
-	};
-
-	clearInputs = () => {
-		this.setState({
-			name: '',
-			date: '',
-			time: '',
-			number: '',
-		});
-	};
-
-	validateInputs = () => {
-		if (!this.state.name) {
-			this.setState({ error: 'You must add a name to the reservation' });
-		}
-
-		if (!this.state.date) {
-			this.setState({ error: 'You must add a date to the reservation' });
-		}
-
-		if (!this.state.time) {
-			this.setState({ error: 'You must add a time to the reservation' });
-		}
-
-		if (!this.state.number) {
-			this.setState({
-				error: 'You must add a guest count of at least 1 to the reservation',
-			});
-		}
-	};
+  submitReservation = () => {
+    const reservation = this.state;
+    this.props.addNewReservation(reservation);
+    this.clearInputs();
+  }
+  
+  clearInputs = () => {
+    this.setState({
+      name: '',
+      date: '',
+      time: '',
+      number: ''
+    })
+  }
 
 	render() {
 		return (
-			<section class='form'>
-				<form className='form'>
-					<label htmlFor='name'></label>
-					<input
-						type='text'
-						name='name'
-						placeholder='Name'
-						value={this.state.name}
-						onChange={this.updateHandler}
-					/>
-					<label htmlFor='date'></label>
-					<input
-						type='text'
-						name='date'
-						placeholder='Date M/D'
-						value={this.state.date}
-						onChange={this.updateHandler}
-					/>
-					<label htmlFor='time'></label>
-					<input
-						type='text'
-						name='time'
-						placeholder='Time H:MM'
-						value={this.state.time}
-						onChange={this.updateHandler}
-					/>
-					<label htmlFor='number'></label>
-					<input
-						type='number'
-						name='number'
-						placeholder='Number of guests'
-						value={this.state.guests}
-						onChange={this.updateHandler}
-					/>
-					<button
-						className='submit-btn'
-						type='button'
-						onClick={this.submitReservation}>
-						Make Reservation
-					</button>
-				</form>
-				{this.state.error && <h1 className='form-error'>{this.state.error}</h1>}
-			</section>
+			<form className='form'>
+				<label htmlFor='name'></label>
+				<input
+					type='text'
+					name='name'
+					placeholder='Name'
+					value={this.state.name}
+					onChange={this.updateHandler}
+				/>
+				<label htmlFor='date'></label>
+				<input
+					type='text'
+					name='date'
+					placeholder='Date M/D'
+					value={this.state.date}
+					onChange={this.updateHandler}
+				/>
+				<label htmlFor='time'></label>
+				<input
+					type='text'
+					name='time'
+					placeholder='Time H:MM'
+					value={this.state.time}
+					onChange={this.updateHandler}
+				/>
+				<label htmlFor='number'></label>
+				<input
+					type='number'
+					name='number'
+					placeholder='Number of guests'
+					value={this.state.guests}
+					onChange={this.updateHandler}
+				/>
+        <button className='submit-btn' type='button' onClick={this.submitReservation}>Make Reservation</button>
+			</form>
 		);
 	}
 }
