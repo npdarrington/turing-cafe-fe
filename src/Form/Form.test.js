9be409251776efb4d48f05dac80a4cab3f5bf907
Nathan Darrington <npdarrington@icloud.com>
2020-10-21 10:47:49 -0600

    A user can type data into form inputs test

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -6,7 +6,19 @@ import Form from './Form';
 
 describe('Form', () => {
   it('Should allow a user to record data into form inputs', () => {
-    
+    render(<Form />);
+    const name = screen.getByPlaceholderText('Name')
+    const date = screen.getByPlaceholderText('Date M/D')
+    const time = screen.getByPlaceholderText('Time H:MM')
+    const guests = screen.getByPlaceholderText('Number of guests')
+    userEvent.type(name, 'Nathan');
+    userEvent.type(date, '10/22');
+    userEvent.type(time, '3:30');
+    userEvent.type(guests, '4');
+    expect(name.value).toBe('Nathan');
+    expect(date.value).toBe('10/22');
+    expect(time.value).toBe('3:30');
+    expect(guests.value).toBe('4');
   });
 
   it('Should allow the user to add a new reservation', () => {
